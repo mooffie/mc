01733007f759d69330a6315638c6079d6bd186bf
Mooffie <mooffie@gmail.com>
2015-12-19 22:42:16 +0200

    195. module utils.table

--- src/lua/library/modules/core/utils.lua
+++ src/lua/library/modules/core/utils.lua
@@ -3,7 +3,8 @@ local M = {}
 
 require('utils.magic').setup_autoload(M)
 
-M.autoload('text', 'utils.text')
+M.autoload('table', 'utils.table')
+ .autoload('text', 'utils.text')
  .autoload('glob', 'utils.glob')
  .autoload('magic', 'utils.magic')
  .autoload('bit32', 'utils.bit32')
