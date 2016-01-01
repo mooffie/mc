a74fd26b30edb4a7f4bfcde3706495df362427d4
Mooffie <mooffie@gmail.com>
2016-01-01 09:10:47 +0200

    45. module devel: pp(), view()
    One of the glaring omissions in Lua is a pretty printer. You just can't
    develop without it. We introduce one here.
    
    E.g., do 'devel.view(obj)' to inspect some object.

--- src/lua/library/modules/core/devel.lua
+++ src/lua/library/modules/core/devel.lua
@@ -7,4 +7,33 @@ require('utils.magic').setup_autoload(devel)
 devel.autoload('ensure', 'devel.ensure')
 devel.autoload('log', { 'devel.log', 'log' })
 
+---
+-- "Pretty prints" a value. It doesn't actually do any printing: it returns a string.
+--
+-- This function can handle complex structures: circular references in
+-- tables are supported.
+--
+-- Example:
+--
+--    devel.log( devel.pp(fs) )
+--
+-- See also @{devel.view}, which you'll more often use.
+--
+-- @function pp
+-- @args (v)
+
+devel.autoload('pp', { 'devel.pp', 'pp' })
+
+---
+-- "Pretty prints" a value to the screen.
+--
+-- Example:
+--
+--    devel.view(_G)
+--
+-- @args (v)
+function devel.view(v)
+  print(devel.pp(v))
+end
+
 return devel
