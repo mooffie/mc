1b30d0ffdff82a49d4bda1f50c01194246819f92
Mooffie <mooffie@gmail.com>
2016-02-22 14:06:56 +0200

    Documentation: explain the different ways to close a dialog.

--- src/lua/tests/nonauto/close_current_dialog.lua
+++ src/lua/tests/nonauto/close_current_dialog.lua
@@ -7,11 +7,35 @@ keymap.bind('C-v', function()
   end
   -- Or we can just do ui.Dialog.top:command("cancel") !
 
-  -- We use :command("cancel") instead of :close() because the later merely
-  -- stops the event loop: it doesn't concern itself with whether the user
-  -- wants to approve or to cancel the action. :command("cancel"), OTOH, also
-  -- marks the dialog as having been canceled (cf. 'h->ret_value = B_CANCEL'
-  -- in C) so the code opening it knows to not carry out the action. Using
-  -- :close() would lead some dialogs (Copy/Move) to think the user has OK'ed
-  -- the action.
+  -- We use :command("cancel"), not :close(). See explanation below.
 end)
+
+--[[
+
+# There are two ways to close a dialog:
+
+* dlg:close()
+* dlg:command("cancel")
+
+Both ways terminate the event loop. But :command("cancel") does something
+further: it sets a flag on the dialog ('h->ret_value = B_CANCEL') that
+tells the system that the user does not wish to carry out the action.
+
+This is important! Using :close() instead would lead some dialogs
+(Copy/Move) to think the user had OK'ed the action.
+
+# So, why does :close() exist? What is it useful for?
+
+It's useful in two cases:
+
+(1) For use in Lua dialogs:
+
+    my_button.on_click = function()
+      dlg.result = "whatever"
+      dlg:close()
+    end
+
+(2) Some built-in MC dialogs (e.g., the viewer) don't respond to
+    CK_Cancel. So you can close them only with :close().
+
+]]
