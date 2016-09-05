46448b103d1d341c7be73f29a0bbed7020a6ccf6
Mooffie <mooffie@gmail.com>
2016-09-05 19:02:02 +0300

    find_file_title.lua: update for newer MC.
    MC no longer has a "Search for content" checkbox in the Find File dialog.

--- src/lua/tests/snippets/find_file_title.lua
+++ src/lua/tests/snippets/find_file_title.lua
@@ -13,24 +13,20 @@ local search_data = nil
 
 ui.Dialog.bind('<<submit>>', function(dlg)
 
-  if dlg.text == T'Find File' then
-    local lbl = dlg:find_label(T'Content:')
-    if lbl then  -- It's the query dialog.
-      search_data = {
-        content = dlg:find('Input', lbl).text,
-        do_search_content = dlg:find('Input', lbl).enabled,  -- Or we can do `dlg:find('Checkbox', lbl).checked`.
-        glob = dlg:find('Input', assert(dlg:find_label(T'File name:'))).text,
-      }
-    end
+  if dlg.text == T'Find File' and dlg:find_label(T'Content:') then  -- It's the query dialog.
+    search_data = {
+      content = dlg:find_labeled_input(T'Content:').text,
+      glob = dlg:find_labeled_input(T'File name:').text,
+    }
   end
 
 end)
 
 ui.Dialog.bind('<<open>>', function(dlg)
 
-  if dlg.text == T'Find File' and search_data then
-    if not dlg:find('Input') then  -- It's the result dialog.
-      if search_data.do_search_content then
+  if dlg.text == T'Find File' and not dlg:find_label(T'Content:') then  -- It's the result dialog.
+    if search_data then
+      if search_data.content ~= '' then
         dlg.text = T"Find File: '%s' Content: '%s'":format(search_data.glob, search_data.content)
       else
         dlg.text = T"Find File: '%s'":format(search_data.glob)
@@ -40,7 +36,15 @@ ui.Dialog.bind('<<open>>', function(dlg)
 
 end)
 
--- Helper.
+
+--
+-- Helpers
+--
+
 function ui.Dialog.meta:find_label(text)
   return self:find('Label', function(l) return l.text == text end)
+end
+
+function ui.Dialog.meta:find_labeled_input(text)
+  return self:find('Input', assert(self:find_label(text)))
 end
